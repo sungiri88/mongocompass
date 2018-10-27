7ece33dc842d765e511947946f3eefdcf50c3ded
umapathy <sungiri88@gmail.com>
2018-10-27 10:24:48 +0000

    Mongo compass choco package.

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = Split-Path $MyInvocation.MyCommand.Definition 
+$packageArgs = @{
+  packageName    = 'mongocompass'
+  fileType       = 'exe'
+  file           = "$toolsDir\mongodb-compass-1.15.4-win32-x64.exe"
+  silentArgs     = '/quiet /qn /norestart'
+  validExitCodes = @(0, 3010)
+}
+Install-ChocolateyInstallPackage @packageArgs
